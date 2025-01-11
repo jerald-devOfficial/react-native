264a5b1dab45aea9c8859613f26afa8f6c7aa5d1
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-11 21:52:50 +0800

    feat: add goalInputHandler and addGoalHandler for state management in TextInput and Button

--- CourseGoal-Blank/App.js
+++ CourseGoal-Blank/App.js
@@ -1,17 +1,29 @@
+import { useState } from 'react'
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your next goal!'/>
-        <Button title='Add Goal'/>
-      </View>
-      <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
-      </View>
-    </View>
-  );
+    const [enteredGoalText, setEnteredGoalText] = useState('')
+    const [courseGoals, setCourseGoals] = useState([])
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText)
+    }
+
+    function addGoalHandler() {
+        setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText])
+    }
+
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}/>
+                <Button title='Add Goal' onPress={addGoalHandler} />
+            </View>
+            <View style={styles.goalsContainer}>
+                {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
