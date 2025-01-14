98e35db003012979aa44d4528d2fee8cf9bfde85
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-15 02:00:41 +0800

    feat(Target-Blank): implement form validation in StartGameScreen and onPress in Primary button, will show alert

--- Target-Blank/screens/StartGameScreen.js
+++ Target-Blank/screens/StartGameScreen.js
@@ -1,8 +1,32 @@
-import { StyleSheet, TextInput, View } from 'react-native'
+import { Alert, StyleSheet, TextInput, View } from 'react-native'
 
+import { useState } from 'react'
 import PrimaryButton from '../components/PrimaryButton'
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState('')
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText)
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber)
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Invalid number!', 'Number has to be between 1 and 99.', [
+        { text: 'Okay', style: 'destructive', onPress: resetInputHandler }
+      ])
+      return
+    }
+
+    console.log('Valid number!')
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber('')
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -11,13 +35,15 @@ function StartGameScreen() {
         keyboardType='number-pad'
         autoCapitalize='none'
         autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
