a9fef8972c533691067d58536584d6178aa513f7
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-24 06:26:12 +0800

    feat(Meals-Blank): show favorite meals in FavoritesScreen

--- Meals-Blank/screens/MealsOverviewScreen.js
+++ Meals-Blank/screens/MealsOverviewScreen.js
@@ -1,7 +1,6 @@
 import { useLayoutEffect } from 'react'
-import { FlatList, StyleSheet, View } from 'react-native'
 
-import MealItem from '../components/MealItem'
+import MealsList from '../components/MealsList/MealsList'
 import { CATEGORIES, MEALS } from '../data/dummy-data'
 
 function MealsOverviewScreen({ route, navigation }) {
@@ -10,20 +9,6 @@ function MealsOverviewScreen({ route, navigation }) {
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0
   })
-
-  function renderMealItem(itemData) {
-    const item = itemData.item
-
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      affordability: item.affordability,
-      complexity: item.complexity,
-      duration: item.duration
-    }
-    return <MealItem {...mealItemProps} />
-  }
 
   useLayoutEffect(() => {
     const categoryTitle = CATEGORIES.find(
@@ -35,22 +20,7 @@ function MealsOverviewScreen({ route, navigation }) {
     })
   }, [catId, navigation])
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  )
+  return <MealsList items={displayedMeals} />
 }
 
 export default MealsOverviewScreen
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16
-  }
-})
