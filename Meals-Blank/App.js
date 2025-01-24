f78d8c30e6bba114d348819ee80cc4c5b5bdce71
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-24 23:58:44 +0800

    feat(Meals-Blank): replace context with redux toolkit

--- Meals-Blank/App.js
+++ Meals-Blank/App.js
@@ -9,7 +9,9 @@ import CategoriesScreen from './screens/CategoriesScreen'
 import FavoritesScreen from './screens/FavoritesScreen'
 import MealDetailScreen from './screens/MealDetailScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen'
-import FavoritesContextProvider from './store/context/favorites-context'
+// import FavoritesContextProvider from './store/context/favorites-context'
+import { Provider } from 'react-redux'
+import { store } from './store/redux/store'
 
 const Stack = createNativeStackNavigator()
 const Drawer = createDrawerNavigator()
@@ -54,7 +56,8 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -83,7 +86,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   )
 }
