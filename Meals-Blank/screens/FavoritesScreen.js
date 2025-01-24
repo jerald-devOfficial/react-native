0d930494abd0e4b2139ed5f6e2b92b8324a20cc1
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-25 00:12:40 +0800

    feat(Meals-Blank): apply rtk state in FavoritesScreen

--- Meals-Blank/screens/FavoritesScreen.js
+++ Meals-Blank/screens/FavoritesScreen.js
@@ -1,15 +1,16 @@
-import { useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import MealsList from '../components/MealsList/MealsList'
 import { MEALS } from '../data/dummy-data'
-import { FavoritesContext } from '../store/context/favorites-context'
+// import { FavoritesContext } from '../store/context/favorites-context'
+import { useSelector } from 'react-redux'
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext)
+  // const favoriteMealsCtx = useContext(FavoritesContext)
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids)
 
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   )
 
   if (favoriteMeals.length === 0) {
