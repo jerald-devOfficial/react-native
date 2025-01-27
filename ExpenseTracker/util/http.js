941ba75c39669900b844838b8b2d1a530cda1b39
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-28 03:35:15 +0800

    feat(ExpenseTracker): transform using fetched data from Firebase

--- ExpenseTracker/util/http.js
+++ ExpenseTracker/util/http.js
@@ -1,7 +1,25 @@
 import axios from 'axios'
 
-const BACKEND_URL = process.env.FIREBASE_URL
+import { BACKEND_URL } from '../constants/env'
 
 export function storeExpense(expenseData) {
-  axios.post(`${BACKEND_URL}/expenses.json`, expenseData)
+  axios.post(BACKEND_URL + '/expenses.json', expenseData)
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expenses.json')
+
+  const expenses = []
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    }
+    expenses.push(expenseObj)
+  }
+
+  return expenses
 }
