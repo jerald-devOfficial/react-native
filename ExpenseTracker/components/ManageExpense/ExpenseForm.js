2ff293a759dcbc0220813697a98f90b640b350b9
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-28 02:27:12 +0800

    feat(ExpenseTracker): manage form state and submission

--- ExpenseTracker/components/ManageExpense/ExpenseForm.js
+++ ExpenseTracker/components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import Button from '../UI/Button'
 import Input from './Input'
 
-function ExpenseForm() {
+function ExpenseForm({ submitButtonLabel, onCancel, onSubmit }) {
   const [inputValues, setInputValues] = useState({
     amount: '',
     date: '',
@@ -11,10 +12,22 @@ function ExpenseForm() {
   })
 
   function inputChangedHandler(inputIdentifier, enteredValue) {
-    setInputValues((prevValues) => ({
-      ...prevValues,
-      [inputIdentifier]: enteredValue
-    }))
+    setInputValues((curInputValues) => {
+      return {
+        ...curInputValues,
+        [inputIdentifier]: enteredValue
+      }
+    })
+  }
+
+  function submitHandler() {
+    const expenseData = {
+      amount: +inputValues.amount,
+      date: new Date(inputValues.date),
+      description: inputValues.description
+    }
+
+    onSubmit(expenseData)
   }
 
   return (
@@ -45,10 +58,20 @@ function ExpenseForm() {
         label='Description'
         textInputConfig={{
           multiline: true,
+          // autoCapitalize: 'none'
+          // autoCorrect: false // default is true
           onChangeText: inputChangedHandler.bind(this, 'description'),
           value: inputValues.description
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode='flat' onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   )
 }
@@ -72,5 +95,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8
   }
 })
