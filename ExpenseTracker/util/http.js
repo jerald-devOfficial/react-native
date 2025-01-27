d38a19fde9b239d2abb2fc66cde309286c138fd0
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-01-28 03:45:52 +0800

    feat(ExpenseTracker): implement update and delete functionality with firebase http methods

--- ExpenseTracker/util/http.js
+++ ExpenseTracker/util/http.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 
 import { BACKEND_URL } from '../constants/env'
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + '/expenses.json', expenseData)
+export async function storeExpense(expenseData) {
+  const response = await axios.post(BACKEND_URL + '/expenses.json', expenseData)
+  const id = response.data.name
+  return id
 }
 
 export async function fetchExpenses() {
@@ -22,4 +24,12 @@ export async function fetchExpenses() {
   }
 
   return expenses
+}
+
+export function updateExpense(id, expenseData) {
+  return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData)
+}
+
+export function deleteExpense(id) {
+  return axios.delete(BACKEND_URL + `/expenses/${id}.json`)
 }
