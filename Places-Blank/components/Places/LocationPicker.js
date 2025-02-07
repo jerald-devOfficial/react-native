65b35c4bb1bd41c48b1c6cb5fd3d8a9957f98d43
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-02-07 23:32:28 +0800

    feat(Places-Blank): add a preview map location

--- Places-Blank/components/Places/LocationPicker.js
+++ Places-Blank/components/Places/LocationPicker.js
@@ -3,12 +3,16 @@ import {
   PermissionStatus,
   useForegroundPermissions
 } from 'expo-location'
-import { Alert, StyleSheet, View } from 'react-native'
+import { useState } from 'react'
+import { Alert, Image, StyleSheet, Text, View } from 'react-native'
 
 import { Colors } from '../../constants/colors'
+import { getMapPreview } from '../../util/location'
 import OutlinedButton from '../UI/OutlinedButton'
 
 function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState()
+
   const [locationPermissionInformation, requestPermission] =
     useForegroundPermissions()
 
@@ -40,14 +44,30 @@ function LocationPicker() {
     }
 
     const location = await getCurrentPositionAsync()
-    console.log(location)
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude
+    })
   }
 
   function pickOnMapHandler() {}
 
+  let locationPreview = <Text>No location picked yet.</Text>
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng)
+        }}
+      />
+    )
+  }
+
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
         <OutlinedButton icon='location' onPress={getLocationHandler}>
           Locate User
@@ -70,11 +90,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.primary100,
-    borderRadius: 4
+    borderRadius: 4,
+    overflow: 'hidden'
   },
   actions: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center'
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+    // borderRadius: 4
   }
 })
