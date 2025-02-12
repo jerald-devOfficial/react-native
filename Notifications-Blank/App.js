18d52a1689046f28bb95819c1002c3c906df559f
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-02-13 01:00:43 +0800

    feat(Notifications-Blank): install expo-notifications, initial scheduleNotificationAsync configuration

--- Notifications-Blank/App.js
+++ Notifications-Blank/App.js
@@ -1,13 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications'
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, View } from 'react-native'
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification',
+        data: { userName: 'John Doe' }
+      },
+      trigger: { seconds: 5 }
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Schedule Notification'
+        onPress={scheduleNotificationHandler}
+      />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
