f696a44ee1ce86b24f7a28b33a927378bd300ca2
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-02-13 21:56:20 +0800

    feat(Notifications-Blank): implement local notification

--- Notifications-Blank/App.js
+++ Notifications-Blank/App.js
@@ -1,8 +1,42 @@
 import * as Notifications from 'expo-notifications'
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
 import { Button, StyleSheet, View } from 'react-native'
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+})
+
 export default function App() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log('NOTIFICATION RECEIVED')
+        console.log(notification)
+        const userName = notification.request.content.data.userName
+        console.log(userName)
+      }
+    )
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log('NOTIFICATION RESPONSE RECEIVED')
+        console.log(response)
+        const userName = response.notification.request.content.data.userName
+        console.log(userName)
+      }
+    )
+
+    return () => {
+      subscription1.remove()
+      subscription2.remove()
+    }
+  }, [])
+
   function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
